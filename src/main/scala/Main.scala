dfa947d3b3e39bee5a9f70d09a6e4844c0ce7a36
Parth Nagraj <pdnagraj@gmail.com>
2018-03-13 19:22:22 -0400

    Data Wrangling with Yelp JSON files

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,176 @@
+import hw.json._
+import hw.wrangling.WranglingLike
+
+object Wrangling extends WranglingLike {
+
+val data: List[Json] = JsonHelper.fromFile("yelp.json")
+
+def key(json: Json, key: String): Option[Json] = json match{
+  case JsonDict(aMap) => aMap.get(JsonString(key))
+  case _ => None
+}
+
+def isFromState(datum: Json, state: String): Boolean = datum match {
+  case JsonDict(aMap) => aMap.get(JsonString("state")) match {
+    case None => false
+    case Some(stateName) => stateName match {
+      case JsonString(x) => x == state
+      case _ => false
+    }
+  }
+  case _ => false
+}
+
+def fromState(data: List[Json], state: String): List[Json] = {
+  data.filter(datum => isFromState(datum, state))
+}
+
+
+def ratingLTHelper(datum: Json, rating: Double): Boolean = datum match {
+  case JsonDict(aMap) => aMap.get(JsonString("stars")) match {
+    case None => false
+    case Some(ratingScore) => ratingScore match{
+      case JsonNumber(f) => (f <= rating)
+      case _ => false
+    }
+  }
+  case _=> false
+}
+  def ratingLT(data: List[Json], rating: Double): List[Json] = {
+    data.filter(datum => ratingLTHelper(datum, rating))
+
+  }
+
+ def ratingGTHelper(datum: Json, rating: Double): Boolean = datum match {
+  case JsonDict(aMap) => aMap.get(JsonString("stars")) match {
+    case None => false
+    case Some(ratingScore) => ratingScore match{
+      case JsonNumber(f) => (f >= rating)
+      case _ => false
+    }
+  }
+  case _=> false
+}
+  def ratingGT(data: List[Json], rating: Double): List[Json] = {
+    data.filter(datum => ratingGTHelper(datum, rating))
+  }
+
+  def containsCategory(alist: List[Json], cate: String): Boolean = alist match{
+    case Nil => false
+    case head :: tail =>{
+       if(head.toString() == JsonString(cate).toString()) true
+       else containsCategory(tail, cate)
+    }
+  } 
+
+
+  def categoryHelper(datum: Json, cate: String): Boolean = datum match {
+    case JsonDict(aMap) => aMap.get(JsonString("categories")) match{
+      case None => false
+      case Some(categoryThing) => categoryThing match {
+        case JsonArray(x) => { 
+        containsCategory(x, cate)}
+        case _ => false
+      }
+    }
+    case _=> false
+  }
+
+
+  def category(data: List[Json], cate: String): List[Json] = { 
+    data.filter(datum => categoryHelper(datum, cate))
+  }
+
+ def groupByState(data: List[Json]): Map[String, List[Json]] =
+  data.groupBy(datum => datum match {
+    case JsonDict(aMap) => aMap.get(JsonString("state")) match {
+      case Some(JsonString(stateCode)) => stateCode
+      case _ => "no state"
+    }
+    case _ => "no state"
+  })
+
+
+
+
+  def groupByCategory(data: List[Json]) = ???
+  
+
+  def getRating(head: Json): Double = { 
+    head match{
+      case JsonDict(aMap) => aMap.get(JsonString("stars")) match {
+        case None => 0
+        case Some(ratingScore) => ratingScore match{
+          case JsonNumber(f) => f
+          case _ => 0.0
+      }
+      case _ => 0.0
+    }
+  case _ => 0.0
+  }
+}
+
+  def getHighestRating(data: List[Json], hNumber: Double): Double = {
+    
+    data match {
+      case Nil => hNumber
+      case head :: tail => {    if(getRating(head) > hNumber){ getHighestRating(tail, getRating(head))} 
+                                else {getHighestRating(tail, hNumber)} }
+    } 
+  }
+
+  def numberOfRatingsHelper(datum: Json, revCount: Double): Boolean = datum match {
+  case JsonDict(aMap) => aMap.get(JsonString("review_count")) match {
+    case None => false
+    case Some(revScore) => revScore match{
+      case JsonNumber(f) => (f >= revCount)
+      case _ => false
+    }
+  }
+  case _=> false
+}
+  def numberOfRatings(data: List[Json]): List[Json] = {
+    data.filter(datum => numberOfRatingsHelper(datum, -1))
+  }
+
+  def bestPlace(data: List[Json]): Option[Json] = {
+   ratingGT(data, getHighestRating(data, -1)) match {
+      case Nil => None
+      case head :: Nil => Some(head)
+      case head :: tail => { numberOfRatings(head :: tail) match {
+              case Nil => None
+              case head :: Nil => Some(head)
+              case head :: tail => Some(head)
+          }
+
+        }
+
+      }
+
+
+    }
+
+  def hasAmbienceHelper(datum: Json, ambience: String): Boolean  =  datum match{
+    case JsonDict(aMap) => aMap.get(JsonString("attributes")) match {
+      case None => false
+      case Some(JsonDict(attribu)) => attribu.get(JsonString("Ambience")) match {
+          case None => false
+            case Some(JsonDict(amb)) => amb.get(JsonString(ambience)) match{
+              case None => false
+              case Some(JsonBool(x)) => x
+              case _=> false  
+            } 
+          case _ => false
+        }
+        case _ => false
+      }
+   case _ => false
+  }
+
+
+  def hasAmbience(data: List[Json], ambience: String): List[Json] = {
+      data.filter(datum => hasAmbienceHelper(datum, ambience))
+  }
+  
+
+}
---
+++ src/test/scala/Tests.scala
@@ -0,0 +1,64 @@
+import Wrangling._
+import hw.json._
+
+// You will need to write more tests.
+class TestSuite extends org.scalatest.FunSuite {
+
+ test("Key"){
+   val json1: Json = JsonDict(Map(JsonString("state") -> JsonString("MA")))
+   assert(key(json1, "state") == Some(JsonString("MA")))
+  } 
+
+  test("fromstate"){
+  val myJson: Json = JsonDict(Map(JsonString("state") -> JsonString("MA")))
+  val moreJson: Json = JsonDict(Map(JsonString("state") -> JsonString("NJ"), JsonString("tate") -> JsonString("J") ))
+  val jsonList: List[Json] = List(myJson, moreJson)
+
+  assert(isFromState(myJson, "MA"))
+  assert(fromState(jsonList, "NJ") == List(moreJson))
+  }
+
+test("rating less than"){
+  val json1: Json = JsonDict(Map(JsonString("stars") -> JsonNumber(4.0) ))
+  val json2: Json = JsonDict(Map(JsonString("stars") -> JsonNumber(5.7) ))
+  val jsonList: List[Json] = List(json1, json2)
+
+  assert(ratingLT(jsonList, 5.6) == List(json1))
+  }
+
+  test("rating Greater than"){
+  val json1: Json = JsonDict(Map(JsonString("stars") -> JsonNumber(4.0) ))
+  val json2: Json = JsonDict(Map(JsonString("stars") -> JsonNumber(5.7) ))
+  val jsonList: List[Json] = List(json1, json2)
+
+  assert(ratingGT(jsonList, 5.6) == List(json2))
+  }
+
+  test("categorize"){
+  
+  val json1: Json = JsonDict(Map(JsonString("categories") -> JsonArray(List(JsonString("Food")))))
+  val jsonList: List[Json] = List(json1)
+
+  assert(category(jsonList, "Food") == List(json1))
+  }
+
+  // test("group by category"){
+  // val json1: Json = JsonDict(Map(JsonString("categories") -> JsonArray(List(JsonString("Food"), JsonString("Water")))))
+  // val json2: Json = JsonDict(Map(JsonString("categories") -> JsonArray(List(JsonString("Lights"), JsonString("Water")))))
+  // val jsonList: List[Json] = List(json1, json2)
+
+  // assert(groupByCategory(jsonList) == Map("Food" -> List(json1), "Water" -> List(json1, json2), "Lights" -> List(json2)))
+  // }
+
+  // test("attributes"){
+
+  // val json1: Json = JsonDict(Map(JsonString("attributes") -> Array(4.0) ))
+
+  // }
+
+  
+
+
+ 
+
+}
